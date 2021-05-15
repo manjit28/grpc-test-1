0d3b21d3b386a51db470ec2f5fbf9a928686dadd
manjit28 <manjit28@users.noreply.github.com>
2021-05-15 10:13:00 -0400

    Add files via upload

---
+++ src/client/Program.cs
@@ -0,0 +1,199 @@
+﻿using Grpc.Core;
+using Product;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Channel channel = new Channel("localhost", 50052, ChannelCredentials.Insecure);
+
+            await channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected successfully");
+            });
+
+            var client = new ProductService.ProductServiceClient(channel);
+            //Product.Product product;
+
+
+            //Test various operations
+            int choice = -1;
+            while (choice != 9)
+            {
+                choice = GetChoice();
+                switch (choice)
+                {
+                    case 9:
+                        break;
+                    case 0:
+                        //Create new product
+                        var p1 = CreateProduct(client);
+                        break;
+                    case 1:
+                        //Create new product
+                        var p2 = GetProduct(client, "product1");
+                        break;
+                    case 2:
+                        //update product
+                        var p3 = GetProduct(client, "product1");
+                        UpdateProduct(client, p3.Product);
+                        break;
+                    case 3:
+                        //delete product
+                        var p4 = GetProduct(client, "product1");
+                        DeleteProduct(client, p4.Product);
+                        break;
+                    case 4:
+                        //get all products
+                        await ListProduct(client);
+                        break;
+                    case 5:
+                        //Test Bidirectional streaming
+                        // test setup with one second delay at client for sending each record 
+                        // and two second delay at server side before saving
+                        await CreateProducts(client);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            //test rpc exception
+            //GetProduct(client, "-1");
+            Console.ReadKey();
+            channel.ShutdownAsync().Wait();
+        }
+
+        private static int GetChoice()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Select Test to do (number and return):");
+            Console.WriteLine("0 : Create Product");
+            Console.WriteLine("1 : Get Product");
+            Console.WriteLine("2 : Update Product");
+            Console.WriteLine("3 : Delete Product");
+            Console.WriteLine("4 : Get All Products (server streaming)");
+            Console.WriteLine("5 : Create Multiple Products (bidirectional streaming)");
+            Console.WriteLine("9 : End Test");
+            Console.WriteLine();
+            int choice = Convert.ToInt32(Console.ReadLine());
+            return choice;
+        }
+        private static Product.Product CreateProduct(ProductService.ProductServiceClient client)
+        {
+            var response = client.CreateProduct(new CreateProductRequest()
+            {
+                Product = new Product.Product()
+                {
+                    Name = "Product X",
+                    Description = "Product X Description"
+                }
+            });
+
+            Console.WriteLine("The Product " + response.Product.Id + " was created !");
+
+            return response.Product;
+        }
+
+        private async static Task CreateProducts(ProductService.ProductServiceClient client)
+        {
+
+            var products = new List<Product.Product>
+            {
+                new Product.Product() {Name = "Product X1", Description = "Product X1 Description" },
+                new Product.Product() {Name = "Product X2", Description = "Product X2 Description" },
+                new Product.Product() {Name = "Product X3", Description = "Product X3 Description" },
+                new Product.Product() {Name = "Product X4", Description = "Product X4 Description" }
+
+            };
+            using (var call = client.CreateProducts())
+            {
+                var requestStream = call.RequestStream;
+                var responseStream = call.ResponseStream;
+
+                var responseTask = Task.Run(async () =>
+                {
+                    while (await responseStream.MoveNext())
+                    {
+                        Console.WriteLine("Saved: " + responseStream.Current.Product);
+                    }
+                });
+
+                foreach (var product in products)
+                {
+                    await requestStream.WriteAsync(new CreateProductRequest() { Product = product });
+                    System.Threading.Thread.Sleep(1000);
+                }
+                await call.RequestStream.CompleteAsync();
+                await responseTask;
+            }
+        }
+
+        private static GetProductResponse GetProduct(ProductService.ProductServiceClient client, string id)
+        {
+            try
+            {
+                var response = client.GetProduct(new GetProductRequest()
+                {
+                    ProductId = id
+                });
+
+                Console.WriteLine(response.Product.ToString());
+
+                return response;
+            }
+            catch (RpcException e)
+            {
+                Console.WriteLine(e.Status.Detail);
+                //just log and continue;
+                Console.ReadKey();
+                return null;
+            }
+        }
+        private static void UpdateProduct(ProductService.ProductServiceClient client, Product.Product Product)
+        {
+            try
+            {
+                var response = client.UpdateProduct(new UpdateProductRequest()
+                {
+                    Product = Product
+                });
+
+                Console.WriteLine(response.Product.ToString());
+            }
+            catch (RpcException e)
+            {
+                Console.WriteLine(e.Status.Detail);
+            }
+        }
+        private static void DeleteProduct(ProductService.ProductServiceClient client, Product.Product Product)
+        {
+            try
+            {
+                var response = client.DeleteProduct(new DeleteProductRequest() { ProductId = Product.Id });
+
+                Console.WriteLine("The Product with id " + Product.Id + " was deleted");
+            }
+            catch (RpcException e)
+            {
+                Console.WriteLine(e.Status.Detail);
+            }
+        }
+        private static async Task ListProduct(ProductService.ProductServiceClient client)
+        {
+            var response = client.GetAllProducts(new GetAllProductsRequest() { });
+
+            while (await response.ResponseStream.MoveNext())
+            {
+                Console.WriteLine(response.ResponseStream.Current.Product.ToString());
+            }
+        }
+    }
+}
---
+++ src/server/ProductServiceImpl.cs
@@ -0,0 +1,150 @@
+﻿using Product;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static Product.ProductService;
+using System.Linq;
+
+namespace server
+{
+    class ProductEntity
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    //Service implementation
+    public class ProductServiceImpl : ProductServiceBase
+    {
+        //just static collection for testing
+        List<ProductEntity> _ProductsDataStore = new List<ProductEntity>()
+        { new ProductEntity{Id = "product1", Name = "Product One", Description = "Product One Description" },
+         new ProductEntity{Id = "product2", Name = "Product Two", Description = "Product Two Description" },
+         new ProductEntity { Id = "product3", Name = "Product Three", Description = "Product Three Description" }
+        };
+
+        public override Task<CreateProductResponse> CreateProduct(CreateProductRequest request, ServerCallContext context)
+        {
+            var Product = request.Product;
+
+            Product.Id = (new Random()).Next().ToString();
+
+            _ProductsDataStore.Add(new ProductEntity()
+            {
+                Id = request.Product?.Id,
+                Name = request.Product?.Name,
+                Description = request.Product?.Description
+            });
+
+            return Task.FromResult(new CreateProductResponse()
+            {
+                Product = Product
+            });
+        }
+
+        public override async Task<GetProductResponse> GetProduct(GetProductRequest request, ServerCallContext context)
+        {
+            var productId = request.ProductId;
+
+            if (productId == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid product id to GetProduct"));
+
+            var result = _ProductsDataStore.FirstOrDefault(predicate => predicate.Id == productId);
+
+            if (result == null)
+                throw new RpcException(new Status(StatusCode.NotFound, $"The Product id {productId} wasn't found"));
+
+            Product.Product Product = new Product.Product()
+            {
+                Id = result.Id,
+                Name = result.Name,
+                Description = result.Description
+            };
+            return await Task.FromResult(new GetProductResponse() { Product = Product });
+        }
+
+        public override async Task<UpdateProductResponse> UpdateProduct(UpdateProductRequest request, ServerCallContext context)
+        {
+            var productId = request.Product?.Id;
+
+            if (productId == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid product id to UpdateProduct"));
+
+            var result = _ProductsDataStore.FirstOrDefault(predicate => predicate.Id == productId);
+
+            if (result == null)
+                throw new RpcException(new Status(StatusCode.NotFound, $"The Product id {productId} wasn't found"));
+
+            result.Name = request.Product?.Name;
+            result.Description = request.Product?.Description;
+
+            //just test, return same product
+            return await Task.FromResult(new UpdateProductResponse() { Product = request.Product });
+        }
+
+        //Bi directional streaming
+        public override async Task CreateProducts(IAsyncStreamReader<CreateProductRequest> requestStream, 
+            IServerStreamWriter<CreateProductResponse> responseStream,
+            ServerCallContext context)
+        {
+            var rnd = new Random();
+            while (await requestStream.MoveNext())
+            {
+                var product = requestStream.Current.Product;
+                Console.WriteLine("Got product:" + product);
+                product.Id = rnd.Next(1000,100000).ToString();
+                _ProductsDataStore.Add(new ProductEntity()
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Description = product.Description
+                });
+                System.Threading.Thread.Sleep(2000);
+                await responseStream.WriteAsync(new CreateProductResponse()
+                {
+                    Product = product
+                });
+            }
+
+            Console.WriteLine("Products saved");
+        }
+
+        public override async Task<DeleteProductRepsponse> DeleteProduct(DeleteProductRequest request, ServerCallContext context)
+        {
+            var productId = request.ProductId;
+
+            if (productId == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid product id to DeleteProduct"));
+
+            var result = _ProductsDataStore.FirstOrDefault(predicate => predicate.Id == productId);
+
+            if (result == null)
+                throw new RpcException(new Status(StatusCode.NotFound, $"The Product id {productId} wasn't found"));
+
+            //remove from data store at this point
+            return await Task.FromResult(new DeleteProductRepsponse() { Status = "Deleted" });
+
+        }
+
+        public override async Task GetAllProducts(GetAllProductsRequest request, IServerStreamWriter<GetAllProductsResponse> responseStream, ServerCallContext context)
+        {
+            var rnd = new Random();
+            foreach (var item in _ProductsDataStore)
+            {
+                System.Threading.Thread.Sleep(rnd.Next(500,1500));
+                await responseStream.WriteAsync(new GetAllProductsResponse()
+                {
+                    Product = new Product.Product()
+                    {
+                        Id = item.Id,
+                        Name = item.Name,
+                        Description = item.Description,
+                    }
+                }) ;
+            }
+        }
+    }
+}
